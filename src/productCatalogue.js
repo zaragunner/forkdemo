fef0b7897a1e2d304c72913a7beb830814d823a0
Laura McBride <20084164@mail.wit.ie>
2021-10-19 10:35:56 +0100

    batchAdd feature developed.

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -33,6 +33,21 @@ class Catalogue {
       .filter((p) => p.quantityInStock <= p.reorderLevel)
       .map((p) => p.id);
     return result;
+  batchAddProducts(batch) {
+    const productIDClash = batch.products.some(
+      (product) => this.findProductById(product.id) !== undefined
+    );
+    if (productIDClash) {
+      throw new Error("Bad Batch");
+    }
+    const noProductsAdded = batch.products
+      .filter((product) => product.quantityInStock > 0 )
+      .filter((p) => {
+        this.addProduct(p);
+        return true;
+      })
+      .reduce((acc, p) => acc + 1, 0);
+    return noProductsAdded;
   }
 }
 module.exports = Catalogue;
